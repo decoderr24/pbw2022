ca2a9bc0cdc769ca4c867a52749fd28e656bd3cd
decoderr24 <113289473+decoderr24@users.noreply.github.com>
2022-10-02 20:25:21 +0700

    Add files via upload

---
+++ MINGGU4/proses.php
@@ -0,0 +1,15 @@
+<?php
+    if (isset($_POST['submit'])){
+        $nisn=$_POST['nisn'];
+        $asalsekolah=$_POST['tempat'];
+        $tahunlulus=$_POST['tahun lulus'];
+        $nama=$_POST['nama'];
+        $nik=$_POST['nik'];
+        $ttl=$_POST['ttl'];
+        $jeniskelamin=$_POST['opsi'];
+        $agama=$_POST['agama'];
+        $alamat=$_POST['alamat'];
+        $email=$_POST['email'];
+        $nomorkk=$_POST['no kk'];
+    }
+?>
